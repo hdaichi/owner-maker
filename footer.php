7ec48c441935250ba04368e70cdd7bcd166b8965
hashioka_daichi <d.hashioka@gmail.com>
2016-10-04 01:23:34 +0900

    初期ファイル準備

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+
+  <div id="footer-nav">
+    <div class="text-center">
+    </div>
+  </div>
+
+  <div class="text-center footer-copyright">Copyright © 2016 OWNER MAKER inc. All Rights Reserved</div>
+
+</div>
+
+</body>
+
+<!--[if lt IE 9]>
+<![endif]-->
+
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+
+<head>
+
+<meta http-equiv="Content-Language" content="ja" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta name="robots" content="" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="viewport" content="width=device-width">
+
+<title>OWNER MAKER</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
