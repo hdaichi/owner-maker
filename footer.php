cf5ea9b6d373b32a5a708f603e9fce5630245bf8
hdaichi <dhassy25@gmail.com>
2016-10-12 02:00:24 +0900

    フッター余白調整

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
 </div><!-- container -->j
 
 <div class="footer">
-  <div id="footer-nav footer-copyright">
-    <div class="text-center">
-    </div>
-  </div>
-
   <div class="text-center footer-copyright">Copyright © 2016 OWNER MAKER inc. All Rights Reserved</div>
 </div>
 
--- footer_index.php
+++ footer_index.php
@@ -1,11 +1,6 @@
 </div><!-- container -->
 
 <div class="footer">
-  <div id="footer-nav footer-index">
-    <div class="text-center">
-    </div>
-  </div>
-
   <div class="text-center footer-index">Copyright © 2016 OWNER MAKER inc. All Rights Reserved</div>
 </div>
 
