1fe0ba87e5086ade7ef7315017f84ca81f9120e9
hdaichi <dhassy25@gmail.com>
2016-10-11 17:55:47 +0900

    テーブル枠追加

--- company.php
+++ company.php
@@ -8,4 +8,54 @@
   <img src="img/company/title_2.png" alt="会社概要" />
 </div>
 
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 10px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-top-width:1px;border-bottom-width:1px;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 10px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-top-width:1px;border-bottom-width:1px;}
+.tg .tg-s6z2{text-align:center; text-justify: 8 ;}
+</style>
+
+<table class="tg">
+  <tr>
+    <th class="tg-s6z2">社名</th>
+    <th class="tg-031e">OWNER MAKER inc</th>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">設立日</td>
+    <td class="tg-031e">2016年xx月</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">資本金</td>
+    <td class="tg-031e">YYYY万円</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">代表者</td>
+    <td class="tg-031e">渕上瑞樹</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">取引銀行</td>
+    <td class="tg-031e">BBB銀行</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">事業内容</td>
+    <td class="tg-031e">1 事業内容1</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2"></td>
+    <td class="tg-031e">2 事業内容2</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2"></td>
+    <td class="tg-031e">3 事業内容3</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2"></td>
+    <td class="tg-031e">4 事業内容4</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2"></td>
+    <td class="tg-031e">5 事業内容5</td>
+  </tr>
+</table>
+
 <?php include 'footer.php'; ?>
