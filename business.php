77711a8e77bd752bef0aa2bcc1adc3723687af8c
hashioka_daichi <d.hashioka@gmail.com>
2016-11-16 00:21:24 +0900

    事業内容、代表挨拶 更新

--- business.php
+++ business.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 <div id="header-img">
-  <img src="img/business/title.png" alt="事業内容" />
+  <img src="img/business/title.png" alt="事業内容詳細" />
 </div>
 
 <div class="img-center contents">
@@ -9,10 +9,50 @@
 </div>
 
 <div class="center contents">
-  音楽イベント運営（年間3講演）
-  1000～2000名クラスの講演会運営（年間30講演）
-  セミナー手配運営（年間200件）
-  その他、イベント協賛多数。
+  <div class="center" style="margin:30px 0;">
+    <table>
+      <tr>
+        <td style="padding-right:40px;">
+          <img src="img/business/business_img01.jpg" width="203" height="135" alt="音楽イベント" />
+        </td>
+        <td style="padding:1px; background-color:#bbb;"></td>
+        <td style="padding-left:40px;">音楽イベント運営<br /><br />年間 3講演以上</td>
+      </tr>
+    </table>
+  </div>
+  <div class="center" style="margin:30px 0;">
+    <table>
+      <tr>
+        <td style="padding-right:40px;">
+          <img src="img/business/business_img02.jpg" width="203" height="135" alt="講演会" />
+        </td>
+        <td style="padding:1px; background-color:#bbb;"></td>
+        <td style="padding-left:40px;">1000～2000名クラスの講演会運営<br /><br />年間 30講演以上</td>
+      </tr>
+    </table>
+  </div>
+  <div class="center" style="margin:30px 0;">
+    <table>
+      <tr>
+        <td style="padding-right:40px;">
+          <img src="img/business/business_img03.jpg" width="203" height="135" alt="セミナー手配" />
+        </td>
+        <td style="padding:1px; background-color:#bbb;"></td>
+        <td style="padding-left:40px;">セミナー手配運営<br /><br />年間 200件以上</td>
+      </tr>
+    </table>
+  </div>
+  <div class="center" style="margin:30px 0;">
+    <table>
+      <tr>
+        <td style="padding-right:40px;">
+          <img src="img/business/business_img04.jpg" width="203" height="135" alt="その他イベント協賛" />
+        </td>
+        <td style="padding:1px; background-color:#bbb;"></td>
+        <td style="padding-left:40px;">その他 イベント協賛 多数</td>
+      </tr>
+    </table>
+  </div>
 </div>
 
 <?php include 'footer.php'; ?>
